ccf69f8035a05cad3303101dd6c790356a80b0df
Andrey <antonov.andrey.10101@gmail.com>
2023-09-14 14:43:48 +0300

    feat: add SocialMedia

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <Logo />
   <Banner />
+  <SocialMedia />
 </template>
 
 <script setup>
 import Logo from "./components/Logo.vue";
 import Banner from "./components/Banner.vue";
+import SocialMedia from "./components/SocialMedia.vue";
+
 </script>
 
 <style scoped>
---
+++ src/components/SocialMedia.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="icons">
+    <a href="/">
+      <i class="bx bxl-github bx-md"></i>
+    </a>
+    <a href="/">
+      <i class="bx bxl-linkedin-square bx-md"></i>
+    </a>
+    <a href="/">
+      <i class="bx bxl-instagram bx-md"></i>
+    </a>
+  </section>
+</template>
+<style scoped>
+.icons {
+  display: flex;
+  gap: 1.5rem;
+}
+
+a {
+  color: var(--blue);
+  border: solid 1px var(--paleBlue);
+  border-radius: 35px;
+  height: 45px;
+  width: 45px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  box-shadow: 0 0 25px var(--paleBlue);
+}
+
+a:hover {
+  color: var(--white);
+  background-color: var(--blue);
+}
+</style>
