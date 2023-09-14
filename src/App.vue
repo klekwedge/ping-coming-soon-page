3611853b72b544656e729e561e6774a7e4e45f19
Andrey <antonov.andrey.10101@gmail.com>
2023-09-14 14:41:20 +0300

    feat: add Banner

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <Logo />
+  <Banner />
 </template>
 
 <script setup>
 import Logo from "./components/Logo.vue";
+import Banner from "./components/Banner.vue";
 </script>
 
 <style scoped>
---
+++ src/components/Banner.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="banner-image">
+    <img
+      src="/images/illustration-dashboard.png"
+      alt="ping illustration dashboard"
+    />
+  </section>
+</template>
+<style scoped>
+.banner-image {
+  max-width: 1000px;
+  padding: 60px 0;
+}
+</style>
